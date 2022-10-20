8d918fb0e8bbb2cf2c6d1e9017a26bf8acd3566b
Conor Gleeson <74910941+ConorGleeson@users.noreply.github.com>
2022-10-20 12:06:19 +0100

    Fix backward/forward arrow

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,14 +5,11 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import Avatar from "@mui/material/Avatar";
-import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useNavigate } from "react-router-dom";
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-  const favouriteMovies = JSON.parse(localStorage.getItem("favourites")); 
-  //pass ids to map and do something with it lmao 
-  //favouriteMovies.find(m => m.id === movie.id)
+  const navigate = useNavigate();
 
   return (
     <Paper 
@@ -25,30 +22,20 @@ const MovieHeader = (props) => {
             margin: 0,
         }}
       >
-      <IconButton aria-label="go back">
+      <IconButton aria-label="go back" onClick={() => navigate(-1)} >
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
-
-         {/* display the heart with an embedded FavoriteIcon or null depending on some condition test. Use the ternary operator to express this, i.e. condition test ? true case : false case */}
-
-      {/*check id of each favourite movie and see if matches current movie if true add heart */}
-         {favouriteMovies.find(m => m.id === movie.id) ? (
-        <Avatar sx={{ backgroundColor: 'red' }}>
-          <FavoriteIcon />
-        </Avatar>
-      ) : null} 
-
 
       <Typography variant="h4" component="h3">
         {movie.title}
         <a href={movie.homepage}>
           <HomeIcon color="primary" />
         </a>
-
         <br />
         <span sx={{ fontSize: "1.5rem" }}>{`   "${movie.tagline}"`} </span>
       </Typography>
-      <IconButton aria-label="go forward">
+
+      <IconButton aria-label="go forward" onClick={() => navigate(+1) } >
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
