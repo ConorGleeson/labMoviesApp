b5e790d1f7adc15dcb415e3b91f030c760da3d2b
Conor Gleeson <74910941+ConorGleeson@users.noreply.github.com>
2022-11-18 12:00:28 +0000

    movied movie components to movie components folder in prepeation for adding tv shows. Fixed all imports

--- src/components/movieComponents/movieCard/index.js
+++ src/components/movieComponents/movieCard/index.js
@@ -11,11 +11,11 @@ import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid";
-import img from '../../images/film-poster-placeholder.png'
+import img from '../../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 
-import { MoviesContext } from "../../contexts/moviesContext";
+import { MoviesContext } from "../../../contexts/moviesContext";
 
 
 
