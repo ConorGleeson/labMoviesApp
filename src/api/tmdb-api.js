e4456767f14b8d31f68ada53ee6ba1faa55f7062
Conor Gleeson <74910941+ConorGleeson@users.noreply.github.com>
2022-11-29 23:45:18 +0000

    Edited login page to look nicer using react and mui components

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -9,7 +9,7 @@ export const getMovies = (args) => {
   const {pageNum} = pageNumPart; 
   console.log(pageNum)
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=${sortMovieBy}&include_adult=true&include_video=false&page=${pageNum}`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=${sortMovieBy}&include_adult=false&include_video=false&page=${pageNum}`
   ).then((response) => {
     if (!response.ok) {
       throw new Error(response.json().message);
@@ -114,7 +114,7 @@ export const getMovieImages = ({ queryKey }) => {
   //tvshow//
   export const getTVShows = () => {
     return fetch(
-      `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_true=true&include_video=false&page=1`
+      `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
     ).then((response) => {
       if (!response.ok) {
         throw new Error(response.json().message);
--- src/pages/userLoginPage.js
+++ src/pages/userLoginPage.js
@@ -1,11 +1,18 @@
 // super basic login page for now // will use firebase https://www.youtube.com/watch?v=9bXhf_TELP4&list=PLojo8XsHDJJoDEKxxijp1j2nw5Qt5QwY7&index=2 video tutorial on firebase setup
-
+import React from "react";
+import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import {createUserWithEmailAndPassword} from 'firebase/auth'
 import {auth} from '../firebase-config.js'
 import {onAuthStateChanged} from 'firebase/auth'
 import {signOut} from 'firebase/auth'
 import {signInWithEmailAndPassword} from 'firebase/auth'
+import Button from '@mui/material/Button';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+import CreateIcon from '@mui/icons-material/Create';
+import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
 
 function LoginPage(){
     const [registerEmail, setRegisterEmail] = useState("");
@@ -53,24 +60,42 @@ function LoginPage(){
     return(
         <div>
             <div>
-                <h1>Register</h1>
-                <input placeholder="Email" onChange={(event) => { setRegisterEmail(event.target.value) }}/>
-                <input placeholder="Password"  onChange={(event) => { setRegisterPassword(event.target.value) }}/>
-                <button onClick={register}>Create User</button>
+            <Typography align="center" variant="h4" sx={{ flexGrow: 1 }}>
+                Register
+                </Typography>
+                <Typography align="center">
+                <TextField id="outlined-basic" label="Email" varient = "outlined"  onChange={(event) => { setRegisterEmail(event.target.value) }}/>
+                <TextField id="outlined-basic" label="Password" varient = "outlined" onChange={(event) => { setRegisterPassword(event.target.value) }}/>
+                </Typography>
+                <Typography  style = {{padding: 20}}  align="center">
+                <Button variant="contained"  color="success" endIcon={<CreateIcon />} onClick={register}>Create User</Button>
+                </Typography>
 
             </div>
             <div>
-                <h1>Login</h1>
-                <input placeholder="Email" onChange={(event) => { setLoginEmail(event.target.value) }}/>
-                <input placeholder="Password" onChange={(event) => { setLoginPassword(event.target.value) }}/>
-                <button onClick={login}>Login</button>
+            <Typography align="center" variant="h4" sx={{ flexGrow: 1 }}>
+                Login
+                </Typography>
+                <Typography align="center">
+                <TextField id="outlined-basic" label="Email" varient = "outlined" onChange={(event) => { setLoginEmail(event.target.value) }}/>
+                <TextField id="outlined-basic" label="Password" varient = "outlined" onChange={(event) => { setLoginPassword(event.target.value) }}/>
+                </Typography>
+                <Typography  style = {{padding: 20}}  align="center">
+                <Button variant="contained" color="success" endIcon={<LoginIcon />} onClick={login}>Login</Button>
+                </Typography>
             </div>
             <div>
-                <h1>User:  </h1>
+            <Typography align="center" variant="h4" sx={{ flexGrow: 1 }}>
+                User:
+                </Typography>
+                <Typography align="center">
                 {user ? user.email : "Not Logged In"}
+                </Typography>
             </div>
             <div>
-                <button onClick={logout}>Sign Out</button>
+            <Typography  style = {{padding: 20}}  align="center">
+                <Button  variant="contained" color="error" endIcon={<LogoutIcon />} onClick={logout}>Sign Out</Button>
+                </Typography>
             </div>
         </div>
     )
